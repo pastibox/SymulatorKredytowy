4f632b5265d6afe1071d3865afa54f797adc443d
Radoslaw Pastuszka <Radoslaw.Pastuszka@unit4.com>
2021-11-13 12:53:24 +0100

    Fixed calculation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,24 +8,42 @@ import { Loan } from './model/loan';
 })
 export class AppComponent implements OnInit {
 
-    title = 'loan-simulator';
+    timeout: any;
+    _loanAmount: number = 400000;
     loanAmountMax: number = 1000000;
     loanAmountMin: number = 10000;
-    _loanAmount: number = 400000;
-
-    timeout: any;
 
     get loanAmount(): number {
         return this._loanAmount;
     }
     set loanAmount(value: number) {
         this._loanAmount = +value;
-        //this.recalculateLoan();
-        this.startTimer();
+        this.startRecalculate();
+    }
+    loanYearsMin: number = 1;
+    loanYearsMax: number = 30;
+    _loanYears: number = 30;
+
+    get loanYears(): number {
+        return this._loanYears;
+    }
+    set loanYears(value: number) {
+        this._loanYears = +value;
+        this.startRecalculate();
     }
 
-    loanYears: number = 30;
-    interestRate: number = 3.25;
+    interestRateMin: number = 0;
+    interestRateMax: number = 30;
+    _interestRate: number = 3.25;
+
+    get interestRate(): number {
+        return this._interestRate;
+    }
+    set interestRate(value: number) {
+        this._interestRate = +value;
+        this.startRecalculate();
+    }
+
     loan: Loan;
 
     symulacja: Loan[] = [];
@@ -41,7 +59,7 @@ export class AppComponent implements OnInit {
         this.recalculateLoan(this.loanAmount, this.loanYears, this.interestRate);
     }
 
-    startTimer() {
+    startRecalculate() {
         clearTimeout(this.timeout);
         this.timeout = setTimeout(() => this.recalculateLoan(this.loanAmount, this.loanYears, this.interestRate), 300);
     }
@@ -59,7 +77,7 @@ export class AppComponent implements OnInit {
     }
 
     formatIntersetRate(value: number) {
-        return value + '%'
+        return value.toFixed(2).replace('.',',') + '%'
     }
 
     recalculateLoan(loanAmount:number, loanYears:number, interestRate:number) {
@@ -125,6 +143,84 @@ export class AppComponent implements OnInit {
 
         return true;
 
+    }
+
+    changeYears(changedYears: number): void {
+        this.loanYears = this.loanYears + changedYears;
+
+        if (this.loanYears < this.loanYearsMin) {
+            this.loanYears = this.loanYearsMin;
+        }
+
+        if (this.loanYears > this.loanYearsMax) {
+            this.loanYears = this.loanYearsMax;
+        }
+    }
+
+    isValidYear(event: any): boolean {
+        const charCode = (event.which) ? event.which : event.keyCode;
+        if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+            return false;
+        }
+        let newValue = +(this.loanYears + String.fromCharCode(charCode));
+        if(newValue > this.loanYearsMax)
+        {
+            return false;
+        }
+
+        return true;
+
+    }
+
+    changePercent(changedPercent: number): void {
+        this.interestRate = +(this.interestRate + changedPercent).toFixed(2);
+
+        if (this.interestRate < this.interestRateMin) {
+            this.interestRate = this.interestRateMin;
+        }
+
+        if (this.interestRate > this.interestRateMax) {
+            this.interestRate = this.interestRateMax;
+        }
+    }
+
+    isValidPercent(event: any): boolean {
+        const charCode = (event.which) ? event.which : event.keyCode;
+    
+        if((charCode >= 48 && charCode <= 57) || charCode == 46 || charCode == 44)
+        {
+            return  true
+        }
+
+        return false;
+
+    }
+
+    changePercentInput(val:any)
+    {
+
+        val.target.value = val.target.value.replace(',','.');
+
+        let changedValue = +(val.target.value);
+
+
+        if(!Number.isNaN(changedValue))
+        {
+            if(changedValue > this.interestRateMin && changedValue < this.interestRateMax)
+            {
+                this.interestRate = changedValue;
+            }
+            else if(changedValue < this.interestRateMin)
+            {
+                this.interestRate = this.interestRateMin;
+            }
+            else if(changedValue > this.interestRateMax)
+            {
+                this.interestRate = this.interestRateMax;
+            } 
+        }
+       
+        val.target.value = this.interestRate;
     }
 
 }
